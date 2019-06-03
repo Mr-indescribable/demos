a3894d9fa411b8e5cfebac75483fde65ef6a900f
Mr-indescribable <st.indescribable@gmail.com>
2019-06-03 15:31:18 +0800

    补充 NLTOU 假想特性

--- Neverland/blueprint_NLTOU.py
+++ Neverland/blueprint_NLTOU.py
@@ -17,15 +17,20 @@ Neverland 私有 TCP Over UDP 协议（NLTOU）初稿
             +--------+   TCP   +----------+   UDP   +--------+   TCP   +------+
 
     2、信道：
-        每个 TCP 连接都被认为是一个 UDP Channel，所有 NLTOU 的 UDP 报文都需要携带
-        Channel 信息，entrance 和 outlet 节点将根据这个 Channel 信息来决定该 UDP
-        报文将被输入到哪个 TCP 流中。
+        每个 TCP 连接都被认为是一个 UDP Channel，所有 NLTOU 的 UDP 报文都需要
+        携带 Channel 信息，entrance 和 outlet 节点将根据这个 Channel 信息来
+        决定该 UDP 报文将被输入到哪个 TCP 流中。
 
-    3、独立的 Worker 和端口：
+    3、少量 TCP 连接：
+        client 与 entrance 之间不应该对每个来自应用的 TCP 连接都建立单独的连接。
+        client 与 entrance 之间只建立少量的连接，传输数据时以数据块的形式传输，
+        并将来自应用的数据通过 channel 区分开来。
+
+    4、独立的 Worker 和端口：
         当开启 NLTOU 支持之后，entrance 以及 outlet 将会启动独立的 Worker 来
         监听 2 个专供 NLTOU 使用的端口，TCP 和 UDP 各一个。
 
-    4、非同步认证：
+    5、非同步认证：
         在 client 与 entrance 建立 TCP 连接之前，client 应当先完成认证。
         此认证过程发生通过常规的 Neverland 通信协议完成，在完成认证之后，
         client 的信息将会被 entrance 记录在共享内存中，并由 TOU Worker
