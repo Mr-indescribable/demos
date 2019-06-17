b92eb9637ba7b3c37d3f68e490723c753bff439a
Mr-indescribable <st.indescribable@gmail.com>
2019-06-17 17:11:48 +0800

    为测试脚本增加说明

--- Neverland/run-test.py
+++ Neverland/run-test.py
@@ -7,6 +7,12 @@ import sys
 
 from pytest import main as pytest_main
 
+
+# I'm not going to put the neverland python package into the
+# python lib directory. So, this script has been written to run
+# all these unit tests directly.
+
+
 if __name__ == '__main__':
     sys.path.append(os.getcwd())
 
